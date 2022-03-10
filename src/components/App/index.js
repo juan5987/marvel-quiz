56d26e8acee58ec278e281ac5d748f87829e29ae
Juan5987 <juan.machado.5987@gmail.com>
2022-03-10 11:06:47 +0100

    added main container

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import 'styles/App.css'
 import Header from 'components/Header'
+import Landing from 'components/Landing'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Landing />
     </div>
   );
 }

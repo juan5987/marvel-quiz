3363e36d6e4caabb8eea77b9afe26f5ee409f6c7
Juan5987 <juan.machado.5987@gmail.com>
2022-03-10 17:30:03 +0100

    added firebase session in Welcome component

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { FirebaseContext } from 'components/Firebase';
 import Logout from 'components/Logout';
 import Quiz from 'components/Quiz';
 
 const Welcome = () => {
 
-  const [userSession, setUserSession] = useState(null);
+  const firebase = useContext(FirebaseContext);
+
+  useEffect(()=> {
+    firebase.auth.onAuthStateChanged( user => {
+      user ? setUserSession(user) : setUserSession("")
+      console.log(userSession);
+    })
+  })
+
+  const [userSession, setUserSession] = useState("");
 
   return (
     <main className='quiz-bg'>

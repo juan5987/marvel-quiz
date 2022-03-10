b1dbf654ad2ef64597fbffe2ff66e8f39c38e826
Juan5987 <juan.machado.5987@gmail.com>
2022-03-10 11:10:53 +0100

    initialized footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className='footer-container'>
+            <p>Projet réalisé par Juan5987 - 2022</p>
+            <p>Les icônes représentant les héros de Marvel ont été récupérées sur le site iconFinder.com</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer

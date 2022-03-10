97dd96ec3c87235058d4004564d491f3aae6a578
Juan5987 <juan.machado.5987@gmail.com>
2022-03-10 16:27:28 +0100

    initialized Logout component

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Logout from 'components/Logout';
 
 const Welcome = () => {
   return (
     <main className='quiz-bg'>
         <div className='container'>
-            Welcome
+            <Logout />
         </div>
     </main>
   )

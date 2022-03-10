93ef0c22681bf4bf60669f1e817b11a6dc2d8b64
Juan5987 <juan.machado.5987@gmail.com>
2022-03-10 12:32:38 +0100

    added errorPage content

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import batman from 'images/batman.png'
 
 const ErrorPage = () => {
     return (
         <div className='quiz-bg'>
             <div className="container">
-                ErrorPage
+                <h2 className='errorPageH2'>Cette page n'existe pas</h2>
+                <img className='errorPageImg' src={batman} alt="error page" />
             </div>
         </div>
     )

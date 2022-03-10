6e01295f12569c1f8f3cfc1578daacf6a2dcc7ac
Juan5987 <juan.machado.5987@gmail.com>
2022-03-10 11:56:41 +0100

    Initialized components: Welcome, Login, Signup, ErrorPage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,12 +2,22 @@ import 'styles/App.css'
 import Header from 'components/Header'
 import Landing from 'components/Landing'
 import Footer from 'components/Footer'
+import Welcome from 'components/Welcome'
+import Login from 'components/Login'
+import Signup from 'components/Signup'
+import ErrorPage from 'components/ErrorPage'
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Welcome />
       <Landing />
+      <Login />
+      <Signup />
+      <ErrorPage />
+      
       <Footer />
     </div>
   );

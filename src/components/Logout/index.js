5337c5de8b444fb7911a4b91b730f15631790949
Juan5987 <juan.machado.5987@gmail.com>
2022-03-10 16:47:51 +0100

    added logout logic

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect, usecontext } from 'react';
+import {FirebaseContext} from 'components/Firebase';
+import { useContext } from 'react';
 
 const Logout = () => {
-  return (
-    <div>
-        Logout
-    </div>
-  )
+
+    const firebase = useContext(FirebaseContext);
+
+    const [checked, setChecked] = useState(false);
+    
+
+    useEffect(() => {
+        if(checked){
+            firebase.signoutUser();
+        }
+    }, [checked, firebase])
+    
+    const handleChange = e => {
+        setChecked(e.target.checked);
+    }
+
+    return (
+        <div className='logoutContainer'>
+            <label className="switch">
+                <input onChange={handleChange} type="checkbox" checked={checked} />
+                <span className='slider round'></span>
+            </label>
+        </div>
+    )
 }
 
 export default Logout;

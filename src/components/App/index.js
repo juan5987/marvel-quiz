0d8b60204a7a2a7b1518886735c947274451ea59
Juan5987 <juan.machado.5987@gmail.com>
2022-03-10 11:03:46 +0100

    added title

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,12 @@
-import 'styles/App.css';
+import 'styles/App.css'
+import Header from 'components/Header'
 
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Header />
     </div>
   );
 }
 
-export default App;
+export default App
